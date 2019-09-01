fc4d0e9a14849cb44ebd3a6e0cd2279a180faf4a
jocelyn <jocelyn.lecomte@gmail.com>
2019-09-01 22:22:32 +0200

    added prettier

--- App.tsx
+++ App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App(): JSX.Element {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
 }
 
 const white = '#fff';
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: white,
-    flex: 1,
-    justifyContent: 'center',
-  },
+    container: {
+        alignItems: 'center',
+        backgroundColor: white,
+        flex: 1,
+        justifyContent: 'center',
+    },
 });
